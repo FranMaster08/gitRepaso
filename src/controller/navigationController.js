be01e9f02005073b9c32dad360c7f5eb13d3eb22
Enoc Soto <tec-enocdejesus@hotmail.com>
2023-12-12 01:27:02 -0300

    add: controller enoc1 and eno2

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -8,6 +8,13 @@ const navigationController = {
   getEnoc: (req, res, next) => {
     res.render("index", { title: "Fran Generator" });
   },
+  getEnoc: (req, res, next) => {
+    res.render("index", { title: "Enoc Generator 1" });
+  },
+
+  getEnoc2: (req, res, next) => {
+    res.render("index", { title: "Enoc Generator 2" });
+  },
 };
 
 module.exports = navigationController;
