338cc075133b9907a1356ca849ebf1833f88cf22
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2023-12-11 23:21:12 -0500

    add : se agrega nuevos metodos a navigation Controller

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -3,6 +3,9 @@ const navigationController = {
     res.render("index", { title: "Fran Generator" });
   },
   getPacho: (req, res, next) => {
+    res.render("index", { title: "Controller Pacho 2" });
+  },
+  getPacho2: (req, res, next) => {
     res.render("index", { title: "Fran Generator" });
   },
 };
