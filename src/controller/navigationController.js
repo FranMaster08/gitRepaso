4a0aa432946926bdcf8fcdf067954676c7e992c3
Enoc Soto <tec-enocdejesus@hotmail.com>
2023-12-12 01:06:16 -0300

    add: controller enoc

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -5,6 +5,9 @@ const navigationController = {
   getPacho: (req, res, next) => {
     res.render("index", { title: "Fran Generator" });
   },
+  getEnoc: (req, res, next) => {
+    res.render("index", { title: "Fran Generator" });
+  },
 };
 
 module.exports = navigationController;
